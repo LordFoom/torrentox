e0c97e11e8436dbcf57506d4387dcdcb2259c2ad
lordfoom <lordfoom@gmail.com>
2024-09-08 16:27:56 +0200

    Start adding arguments
    We will build on thie structures and functions we develope
    while implementing a list of files from the cli.

--- src/args.rs
+++ src/args.rs
@@ -1,5 +1,8 @@
 ///CLI arguments we can pass the application
+#[derive(Debug, Parser)]
 struct AppArgs {
     ///List of arguments we can give
     //the reviled java programmer
+    #[arg(s)]
+    torrent_files: Vec<String>,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 mod parser;
 
+///TODO add torrent file parsing
+///TODO add peer retrieval
+///TODO add peer connection
+///TODO add downloading from peer
+///TODO seeding
+///TODO resume downloading partially downloaded file
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, TorrentOx!");
 }
