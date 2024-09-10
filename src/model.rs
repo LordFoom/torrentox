e0ac5935226ef4cb4b8861103cb5bb02f0bb31a2
lordfoom <lordfoom@gmail.com>
2024-09-10 17:15:29 +0200

    Another field

--- src/model.rs
+++ src/model.rs
@@ -3,6 +3,7 @@ use serde_derive::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Info {
     name: Option<String>,
+    piece_length: usize,
 }
 pub struct TorrentFile {
     announce: String,
