70e5e93e0f16424bfd391a05976bc445dc5d134c
Timothy Spring <foom@amazon.com>
2024-10-03 15:13:09 +0200

    Unit test works
    But we have a useless model :)
    Now to keep adding fields until we have something useful.
    Then, a tui.

--- src/model.rs
+++ src/model.rs
@@ -3,11 +3,11 @@ use serde_derive::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Info {
     pub name: Option<String>,
-    pub piece_length: usize,
+    pub piece_length: Option<usize>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct TorrentFile {
-    pub announce: String,
+    pub announce: Option<String>,
     pub info: Info,
 }
