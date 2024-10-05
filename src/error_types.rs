ad562c32690a72d72210ad0e3e0f295f3a162692
lordfoom <lordfoom@gmail.com>
2024-10-05 23:14:51 +0200

    Start impl format for error type

--- src/error_types.rs
+++ src/error_types.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use serde_bencode::Error as BencodeError;
 use thiserror::Error;
 
@@ -7,4 +9,10 @@ pub enum TorrentParseError {
     BenCode(#[from] BencodeError),
     #[error("Failed to read the torrent file")]
     CannotOpenFile(#[from] std::io::Error),
+}
+
+impl Display for TorrentParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
 }
