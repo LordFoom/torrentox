198d62a01cef92635a5102004ba0b6fcaf89f909
lordfoom <lordfoom@gmail.com>
2024-10-06 18:13:56 +0200

    Display impl for error type

--- src/error_types.rs
+++ src/error_types.rs
@@ -13,6 +13,9 @@ pub enum TorrentParseError {
 
 impl Display for TorrentParseError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        todo!()
+        let msg = match self {
+            TorrentParseError::BenCode(why) => format!("Failed to decode the file {why}"),
+        };
+        write!(f, "{msg}")
     }
 }
