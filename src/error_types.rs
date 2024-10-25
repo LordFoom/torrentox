05a464f97b6a38c1bfca252e09ea47ada02cb1fa
lordfoom <lordfoom@gmail.com>
2024-10-25 18:20:30 +0200

    Expand error types
    I will successfully return an error when listning torrent files from the
    db,
    i will not let you defeat me rusqlite error type!

--- src/error_types.rs
+++ src/error_types.rs
@@ -7,3 +7,11 @@ pub enum TorrentParseError {
     #[error("Failed to read the torrent file")]
     CannotOpenFile(#[from] std::io::Error),
 }
+
+#[derive(Debug, thiserror::Error)]
+pub enum DbError {
+    #[error("Database error: {0}")]
+    Database(#[from] rusqlite::Error),
+    #[error("Deserializetion error: {0}")]
+    Deserializetion(#[from] serde_bencode::Error),
+}
